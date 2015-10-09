145c84a04d7166fa0764c5d484ac2ef7641f647b
Jon Gunderson <jongund@illinois.edu>
2015-10-09 10:55:40 -0500

    Updated some files

--- fae2/static/js/fae2.js
+++ fae2/static/js/fae2.js
@@ -1,1 +1,5 @@
- 
+// Update the title of the page with H1 header content
+window.addEventListener('load', function() {
+    var h1 = document.getElementsByTagName('h1')[0]
+    window.document.title = h1.textContent + ": " + window.document.title;
+});
