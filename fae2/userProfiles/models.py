f3e29d7b9e1d27a9acc39b53866cf70120d9f5a8
Jon Gunderson <jongund@illinois.edu>
2016-02-08 21:26:37 -0600

    update userProfile to make ws_report_groups optional

--- fae2/userProfiles/models.py
+++ fae2/userProfiles/models.py
@@ -47,7 +47,7 @@ class UserProfile(models.Model):
     website_authorization_enabled   = models.BooleanField(default=False)
     advanced_enabled                = models.BooleanField(default=False)
 
-    ws_report_group = models.OneToOneField(WebsiteReportGroup)
+    ws_report_group = models.OneToOneField(WebsiteReportGroup, blank=True)
   
     def __unicode__(self):
         return self.user.username  
