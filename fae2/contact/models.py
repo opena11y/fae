f24b9524603ef36cf8f3d4fbe9cead576ef62ce6
Jon Gunderson <jongund@illinois.edu>
2016-02-22 15:29:42 -0600

    updated comment form

--- fae2/contact/models.py
+++ fae2/contact/models.py
@@ -7,13 +7,24 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+CONTACT_STATUS = (
+    ('NR',  'No Response'),
+    ('FB',  'Filed bug'),
+    ('FFR', 'Filed feature enhancment'),
+    ('R',   'Responded'),
+)
+
 # User Contact (associated with contact form) 
 class Contact(models.Model):
     id       = models.AutoField(primary_key=True)
     date     = models.DateTimeField(auto_now_add=True)
-    user     = models.ForeignKey(User, editable=True)
+    user     = models.ForeignKey(User, editable=True, related_name="contacts")
     topic    = models.CharField(max_length=1024)
     message  = models.TextField()
-    status   = models.CharField(max_length=128)
-    comments = models.TextField()
+    status   = models.CharField(max_length=128, choices=CONTACT_STATUS, default="NR")
+    comments = models.TextField(blank=True, default="")
     
+    def show_status(self):
+        for shortp, longp in CONTACT_STATUS:
+            if shortp == self.status:
+                return longp
