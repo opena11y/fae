2dd2895cf1e347543cf9b41402572cc1342e2628
Jon Gunderson <jongund@illinois.edu>
2016-03-31 12:09:31 -0500

    shib debugging

--- fae2/fae2/base.py
+++ fae2/fae2/base.py
@@ -145,6 +145,7 @@ if SHIBBOLETH_ENABLED:
 
     AUTHENTICATION_BACKENDS = (
         'django.contrib.auth.backends.ModelBackend',
+        'django.contrib.auth.backends.RemoteUserBackend',
         'shibboleth.backends.ShibbolethRemoteUserBackend',
     )
 
