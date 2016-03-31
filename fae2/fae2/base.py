3dec185156dd5996b124a2f07013ee9250fd2943
Jon Gunderson <jongund@illinois.edu>
2016-03-31 10:11:14 -0500

    update

--- fae2/fae2/base.py
+++ fae2/fae2/base.py
@@ -165,6 +165,7 @@ else:
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.RemoteUserMiddleware',    
         'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
