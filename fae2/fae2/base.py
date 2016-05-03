d1437ab3756a871219ab800985df4401bd49a434
Jon Gunderson <jongund@illinois.edu>
2016-05-03 13:23:14 -0500

    update middleware includes

--- fae2/fae2/base.py
+++ fae2/fae2/base.py
@@ -142,15 +142,13 @@ if SHIBBOLETH_ENABLED:
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-        'django.contrib.auth.middleware.PersistentRemoteUserMiddleware',
+        'shibboleth.middleware.ShibbolethRemoteUserMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
         'django.middleware.security.SecurityMiddleware',
-        'shibboleth.middleware.ShibbolethRemoteUserMiddleware',
     )
 
     AUTHENTICATION_BACKENDS = (
-        'django.contrib.auth.backends.RemoteUserBackend',
         'shibboleth.backends.ShibbolethRemoteUserBackend',
     )
 
