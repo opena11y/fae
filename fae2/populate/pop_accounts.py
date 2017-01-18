722bb92fc020bc9264ea08921d3fa8dd54a31a7d
Jon Gunderson <jongund@illinois.edu>
2017-01-17 19:36:20 -0600

    updates

--- fae2/populate/pop_accounts.py
+++ fae2/populate/pop_accounts.py
@@ -114,31 +114,20 @@ def set_subscription_rate(type_id, desc, one, three, six, twelve):
 
 
 at00 = create_acount_type(0, False, False, False, False,  'Anonymous',         1,  1, 1,    1, False,  False, False)
-at01 = create_acount_type(1, True, False, False, False,   'Free',               2,  5, 2,   10, True,   False, False)
+
+at01 = create_acount_type(1, True, False, False, False,   'Free',               5,  10, 3,   25, True,   False, False)
 at00.next_account_type = at01
 at00.save()
 
-at02 = create_acount_type(2, True, False, False, False,   'Standard I',         5, 10, 3,   25, False,  False, False)
+at02 = create_acount_type(2, True, False, False, False,   'Standard',       20, 40, 4,   100, False,  False, False)
 at01.next_account_type = at02
 at01.save()
 
-at03 = create_acount_type(3, True, False, False, False,   'Standard II',       10, 20, 3,   50, False,  False, False)
+at03 = create_acount_type(3, True, False, False, False,   'Advanced',        40, 80, 5,  400, False,   True, False)
 at02.next_account_type = at03
 at02.save()
 
-at04 = create_acount_type(4, True, False, False, False,   'Advanced I',        10, 20, 4,  100, False,   True, False)
-at03.next_account_type = at04
-at03.save()
-
-at05 = create_acount_type(5, True, False, False, False,   'Advanced II',       20, 40, 5,  200, False,   True, False)
-at04.next_account_type = at05
-at04.save()
-
-at06 = create_acount_type(6, True, False, False, False,   'Advanced III',      40, 80, 6,  400, False,   True, False)
-at05.next_account_type = at06
-at05.save()
-
-at16 = create_acount_type(16, False, True, False, False, 'Institutional Free',  2,  5, 3,   20, False, False, False)
+at16 = create_acount_type(16, False, True, False, False, 'Institutional Free',  5, 10, 3,   50, False, False, False)
 at17 = create_acount_type(17, False, True, False, False, 'Institutional I',    20, 40, 4,  100, False,  True, False)
 at18 = create_acount_type(18, False, True, False, False, 'Institutional II',   20, 40, 4,  100, False,  True, False)
 at19 = create_acount_type(19, False, True, False, False, 'Institutional III',  20, 40, 4,  100, False,  True, False)
@@ -152,11 +141,8 @@ a65 = create_acount_type(65, False, False, False, True, 'Special II',  40, 80, 4
 a66 = create_acount_type(66, False, False, False, True, 'Special III', 40, 80, 4, 100, False, True, False)
 
 set_account_type_description(1, """The Free version provides basic website evaluation for any user.  If you use FAE and/or AInspector Sidebar on a regular basis we ask that you help support the development and maintenance of the tools by purchasing a paid level subscriptions.""")
-set_account_type_description(2, """The Standard I subscription provides an inidividual, educational and/or other organization with more extensive website evaluation services including deeper spidering of pages in a website, higher maximum number of pages, more saved and permanent reports than the Free account type.""")
-set_account_type_description(3, """The Standard II subscription provides an inidividual, educational and/or other organization with more extensive website evaluation services including deeper spidering of pages in a website, higher maximum number of pages, more saved and permanent reports than the Standard I subscription.""")
-set_account_type_description(4, """The Advanced I subscription provides an inidividual, educational and/or other organization with more extensive website evaluation services including deeper spidering of pages in a website, higher maximum number of pages, more saved and permanent reports than the Standard II subscription and also includes more advanced control over website spidering through the specification of include and exclude domain names.""")
-set_account_type_description(5, """The Advanced II subscription provides an inidividual, educational and/or other organization with more extensive website evaluation services including deeper spidering of pages in a website, higher maximum number of pages, more saved and permanent reports than the Advanced I subscription and also includes more advanced control over website spidering through the specification of include and exclude domain names.""")
-set_account_type_description(6, """The Advanced III subscription provides an inidividual, educational and/or other organization with more extensive website evaluation services including deeper spidering of pages in a website, higher maximum number of pages, more saved and permanent reports than the Advanced II subscription and also includes more advanced control over website spidering through the specification of include and exclude domain names.""")
+set_account_type_description(2, """The Standard subscription provides an inidividual, educational and/or other organization with more extensive website evaluation services including deeper spidering of pages in a website, higher maximum number of pages, more saved and permanent reports than the Free account type.""")
+set_account_type_description(3, """The Advanced subscription provides an inidividual, educational and/or other organization with more extensive website evaluation services including deeper spidering of pages in a website, higher maximum number of pages, more saved and permanent reports than the Standard II subscription and also includes more advanced control over website spidering through the specification of include and exclude domain names.""")
   
 set_account_type_description(16, """Free version for educational institutions.""")
 set_account_type_description(17, """For educational institutions of less than 2,000 students.""")
@@ -167,10 +153,7 @@ set_account_type_description(21, """For educational institutions of more than 15
 
 set_subscription_rate(1, "",    0,    0,    0,    0)
 set_subscription_rate(2, "",   30,   81,  153,  270)
-set_subscription_rate(3, "",   40,  108,  204,  360)
-set_subscription_rate(4, "",  120,  324,  612, 1080)
-set_subscription_rate(5, "",  200,  540, 1020, 1800)
-set_subscription_rate(6, "",  300,  810, 1530, 2700)
+set_subscription_rate(3, "",  120,  324,  612, 1080)
 
 set_subscription_rate(16, "",    0,    0,    0,    0)
 set_subscription_rate(17, "",    0,  540, 1020, 1800)
