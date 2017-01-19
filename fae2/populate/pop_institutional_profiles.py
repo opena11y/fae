b11bf9bd79f67b458f139442795c9a3d5104c67c
Jon Gunderson <jongund@illinois.edu>
2017-01-19 12:30:07 -0600

    update

--- fae2/populate/pop_institutional_profiles.py
+++ fae2/populate/pop_institutional_profiles.py
@@ -93,4 +93,5 @@ create_institutional_profile(21,'Iowa State University','iastate', '','Zayira Jo
 create_institutional_profile(21,'Southern Illinois University','siu', 'siue','Elizabeth Hunter','lizhunter@siu.edu','', '', '', '', '2017-01-05', '2017-03-01')
 create_institutional_profile(21,'Purdue University','purdue', '','Dean Brusnighan','dabrus@purdue.edu','', '', '', '', '2017-01-05', '2017-03-01')
 create_institutional_profile(21,'University of Montana','umt', 'umontana','Ryan Snyder]','Ryan.Snyder@umontana.edu]','(406) 243-6321', '', '', '', '2017-01-05', '2018-02-01')
+create_institutional_profile(21,'Temple','temple', '','','','', '', '', '', '2017-01-05', '2017-03-01')
 
