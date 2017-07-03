c58e16ca2359bc2211b4b3fee67f1b317ddce0be
Jon Gunderson <jongund@illinois.edu>
2017-07-02 21:01:34 -0500

    Update and rename fae2-cron-cleanup.sh to fae-util-report-cleanup.sh

---
+++ scripts/fae-util-report-cleanup.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/var/www/fae2/fae2env/bin/python /var/www/fae2/fae2/fae2/fae-util/process_archived_reports.py
--- scripts/fae2-cron-cleanup.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-/var/www/fae2/virtual-environments/fae-self-reg-production/bin/python /var/www/fae2/fae-self-reg-production/fae2/fae-util/process_archived_reports.py
