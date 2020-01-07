e329badb9814066836069968a33c233dbb666e66
Jon Gunderson <jongund@illinois.edu>
2020-01-07 16:37:24 -0600

    user info utility

---
+++ fae2/populate/user_info.py
@@ -0,0 +1,56 @@
+"""
+Copyright 2014-2016 University of Illinois
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+file: populate/pop_site.py
+
+Author: Jon Gunderson
+
+"""
+
+from __future__ import print_function
+from __future__ import absolute_import
+import sys
+import os
+import django
+from django.core.exceptions import ObjectDoesNotExist
+import json
+
+fp = os.path.realpath(__file__)
+path, filename = os.path.split(fp)
+
+fae2_path = path.split('/populate')[0]
+sys.path.append(fae2_path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fae2.settings')
+from django.conf import settings
+
+django.setup()
+
+"""This file is for populating the database with markup information
+I empty it. Run as a standalone script!"""
+
+from django.core.exceptions      import ObjectDoesNotExist
+from django.core.exceptions      import ImproperlyConfigured
+from django.contrib.sites.models import Site
+from django.contrib.auth.models  import User
+from accounts.models             import AccountType
+from userProfiles.models         import UserProfile
+from websiteResultGroups.models  import WebsiteReportGroup
+from stats.models                import StatsUser
+from stats.models                import StatsRegisteredUsers
+
+
+for u in User.objects.all():
+  print('username: ' + u.username)
